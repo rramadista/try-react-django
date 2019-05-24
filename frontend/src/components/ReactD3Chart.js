a382c2ac5ce00b7fe7e01da3932d9b1e893d5272
rramadista <raden.ramadista@gmail.com>
2019-05-24 16:16:25 +0700

    try add dataviz

--- frontend/src/components/ReactD3Chart.js
+++ frontend/src/components/ReactD3Chart.js
@@ -1,24 +1,89 @@
 import React, { Component } from 'react';
-// import * as d3 from 'd3';
+import * as d3 from 'd3';
 
+let expenses = [
+    {
+        name: 'Coffee',
+        amount: 4,
+        date: new Date()
+    }, {
+        name: 'Tea',
+        amount: 46,
+        date: new Date()
+    }, {
+        name: 'Cola',
+        amount: 40,
+        date: new Date()
+    }, {
+        name: 'Milk',
+        amount: 15,
+        date: new Date()
+    }
+]
 
-class BarChart extends Component {
+let width = 1100;
+let height = 400
+let radius = 20;
+let simulation = d3.forceSimulation()
+    .force('center', d3.forceCenter(width / 2, height / 2))
+    .force('change', d3.forceManyBody())
+    .force('collide', d3.forceCollide(radius))
+    .stop();
 
-    componentDidMount() {
-        const data = [2, 4, 2, 6, 8]
-        this.drawBarChart(data)
+class DataVizExample extends Component {
+
+    constructor(props) {
+        super(props);
+        this.forceTick = this.forceTick.bind(this);
     }
 
-    drawBarChart(data) {
+    componentWillMount() {
+        simulation.on('tick', this.forceTick);
+    }
 
+    componentDidMount() {
+        this.container = d3.select(this.refs.container);
+        this.renderCircles();
+
+        simulation
+            .nodes(expenses)
+            .alpha(0.8)
+            .restart();
+    }
+
+    componentDidUpdate() {
+        this.renderCircles();
+    }
+
+    renderCircles() {
+        this.circles = this.container.selectAll('circle')
+            .data(expenses, d => d.name);
+        this.circles
+            .exit()
+            .remove();
+        this.circles = this.circles
+            .enter()
+            .append('circle')
+            .merge(this.circles)
+            .attr('r', radius)
+            .attr('opacity', 0.5)
+            .attr("fill", "yellow")
+            .attr("stroke", "black")
+            .attr("stroke-width", d => d / 2);
+    }
+
+    forceTick() {
+        this.circles
+            .attr('cx', d => d.x)
+            .attr('cy', d => d.y)
     }
 
     render() {
         return (
-            <div ref="canvas">
+            <svg width={width} height={height} ref="container">
 
-            </div>
+            </svg>
         )
     }
 }
-export default BarChart;
+export default DataVizExample;
--- frontend/src/containers/DataVizView.js
+++ frontend/src/containers/DataVizView.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import BarChart from '../components/ReactD3Chart';
+import DataVizExample from '../components/ReactD3Chart';
 
 const DataViz = props => (
     <div>
         <h1>Data Visualization</h1>
         <p>This is a simple example of a React Org Chart component.</p>
 
-        <BarChart />
+        <DataVizExample />
 
     </div>
 );
